76b2348711a63f5c80c3af1f6b7174d7259c4c6f
RavensCall <rc@ravenscall.tail720429.ts.net>
2026-09-06 21:45:39 -0700

    Fix fatal error: remove dangling require for deleted WooCommerce compat file

--- wp-content/themes/MariasPlace/inc/plugin-compatibility/plugin-compatibility.php
+++ wp-content/themes/MariasPlace/inc/plugin-compatibility/plugin-compatibility.php
@@ -3,8 +3,3 @@
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/plugin-compatibility/jetpack/jetpack.php';
-
-/**
- * Load Woocommerce compatibility file.
- */
-require get_template_directory() . '/inc/plugin-compatibility/woocommerce/woocommerce.php';
